44678a5f499b09d8f6c4ac1fcd28115e63ae9882
huy <nomnom1204@gmail.com>
2019-04-03 14:47:27 +0700

    Initial Commit

---
+++ RatingStar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RatingStar
+//
+//  Created by huy on 03/04/2019.
+//  Copyright © 2019 huy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
